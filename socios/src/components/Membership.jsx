031d33a8478e5302790ddafc3cc8849d8eb264db
Ignacio <ignaciocoriadb@gmail.com>
2024-02-19 11:47:09 -0300

    dateFormat

--- socios/src/components/Membership.jsx
+++ socios/src/components/Membership.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllMembership } from '../redux/membershipActions/actions';
+import { format } from 'date-fns';
 
 const Membership = () => {
   const dispatch = useDispatch();
@@ -14,17 +15,40 @@ const Membership = () => {
 
 
   return (
-    <div>
+    <div className='container'>
       <h1>Lista de socios</h1>
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
-      {membershipData && (
-        <div>
-          {membershipData.map((u, key) => (
-            <p key={key}>{u.Person.DNI}</p>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Apellido</th>
+            <th scope="col">Fecha de Nacimiento</th>
+            <th scope="col">Dirección</th>
+            <th scope="col">Teléfono</th>
+            <th scope="col">Dni</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+        {membershipData?.map((u, key) => (
+          <tr key={key}>
+            <td >{u.Person.name}</td>
+            <td >{u.Person.lastName}</td>
+            <td >{format(u.Person.dateOfBirth, 'dd/MM/yyyy')}</td>
+            <td >{u.Person.address}</td>
+            <td >{u.Person.phone}</td>
+            <td >{u.Person.DNI}</td>
+            <td >{u.Person.email}</td>
+          </tr>
           ))}
-        </div>
-      )}
+        
+        
+        </tbody>
+      </table>
+    
   
     </div>
   );
