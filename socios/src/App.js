b51f5d88976cd1bf8a17874d73ab0d9f54a320d2
Ignacio <ignaciocoriadb@gmail.com>
2024-02-20 14:07:08 -0300

    route people

--- socios/src/App.js
+++ socios/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Membership from './components/membership/Membership';
+import People from "./components/people/allPeople";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <header>
       <Routes>
         <Route path="/membership" element={<Membership />} />
+        <Route path="/people" element={<People />} />
+
       </Routes>
       </header>
     </div>
